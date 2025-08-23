f2f7546f2ec8679aa2f5fed4e7543f963b078057
mdarsathbmtechx-gif <mdarsath.bmtechx@gmail.com>
2025-08-23 13:40:04 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,10 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(),tailwindcss()],
   build: {
-    outDir: "dist"},
+    outDir: "dist",
+  },
 })
