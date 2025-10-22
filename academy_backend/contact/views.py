009ad1a109ac96a0428459c74290606937cc682d
mdarsathbmtechx-gif <mdarsath.bmtechx@gmail.com>
2025-10-22 13:01:34 +0530

    contact views.py

--- academy_backend/contact/views.py
+++ academy_backend/contact/views.py
@@ -15,18 +15,18 @@ class ContactMessageCreateView(generics.CreateAPIView):
         instance = serializer.save()
 
         # Send email notification
-        send_mail(
-            subject=f"New Contact Message: {instance.subject or 'No Subject'}",
-            message=f"""
-You have a new contact form submission:
+#         send_mail(
+#             subject=f"New Contact Message: {instance.subject or 'No Subject'}",
+#             message=f"""
+# You have a new contact form submission:
 
-Name: {instance.name}
-Email: {instance.email}
-Subject: {instance.subject}
-Message:
-{instance.message}
-""",
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[settings.EMAIL_HOST_USER],
-            fail_silently=False,
-        )
+# Name: {instance.name}
+# Email: {instance.email}
+# Subject: {instance.subject}
+# Message:
+# {instance.message}
+# """,
+#             from_email=settings.DEFAULT_FROM_EMAIL,
+#             recipient_list=[settings.EMAIL_HOST_USER],
+#             fail_silently=False,
+#         )
