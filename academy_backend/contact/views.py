fd92b99292d9a896929abff5dee47690068a3f9e
mdarsathbmtechx-gif <mdarsath.bmtechx@gmail.com>
2025-10-22 12:08:24 +0530

    contact

--- academy_backend/contact/views.py
+++ academy_backend/contact/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import ContactMessage
 from .serializers import ContactMessageSerializer
 
 class ContactMessageCreateView(generics.CreateAPIView):
     queryset = ContactMessage.objects.all()
     serializer_class = ContactMessageSerializer
+    permission_classes = [permissions.AllowAny]  # 👈 Add this line
